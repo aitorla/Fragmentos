bb380b9ad0e889679f616d56f05580413cbc1dcc
aitor <aitor@kaixo.com>
2015-10-21 21:52:56 +0200

    v2

--- app/src/main/java/com/example/aitor/fragmentos/DetalleActivity.java
+++ app/src/main/java/com/example/aitor/fragmentos/DetalleActivity.java
@@ -1,6 +1,6 @@
 package com.example.aitor.fragmentos;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
 public class DetalleActivity extends AppCompatActivity   {
 
     public static final String EXTRA_TEXTO = "com.example.aitor.fragmentos.EXTRA_TEXTO";
--- app/src/main/java/com/example/aitor/fragmentos/FragmentDetalle.java
+++ app/src/main/java/com/example/aitor/fragmentos/FragmentDetalle.java
@@ -1,25 +1,24 @@
 package com.example.aitor.fragmentos;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 public class FragmentDetalle extends Fragment   {
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater,
-			                 ViewGroup container,
-			                 Bundle savedInstanceState) {
-		
-		return inflater.inflate(R.layout.fragment_fragment_detalle, container, false);
-	}
-	
-	public void mostrarDetalle(String texto) {
-		TextView txtDetalle = (TextView)getView().findViewById(R.id.TxtDetalle);
-		
-		txtDetalle.setText(texto);
-	}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_fragment_detalle, container, false);
+    }
+
+    public void mostrarDetalle(String texto) {
+        TextView txtDetalle = (TextView)getView().findViewById(R.id.TxtDetalle);
+
+        txtDetalle.setText(texto);
+    }
 }
+
